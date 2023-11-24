7be87ab9b166cdb59fe2fad8757a3e1bd8bd8eff
win/ubuntu <d-brandenburg@hotmail.de>
2023-11-24 14:59:13 +0100

    fixed the prograssbar to a slider

--- scripts/Progress__Bar.js
+++ scripts/Progress__Bar.js
@@ -1,20 +1,33 @@
 const audioElement = document.getElementById("PlayerAudioSrc");
-const progressBar = document.getElementById("Progress__Bar");
+const seekSlider = $("#Slider_progress"); // Use jQuery to select the slider
 
 audioElement.addEventListener("loadedmetadata", function () {
   const duration = audioElement.duration;
   console.log("Audio Duration:", duration);
 
+  // Initialize the slider with the 'value' option
+  seekSlider.slider({
+    min: 0,
+    max: 100,
+    value: 0, // Initial value
+    range: "min",
+    slide: function (event, ui) {
+      const seekTime = (ui.value / 100) * duration;
+      audioElement.currentTime = seekTime;
+    },
+  });
+
+  // Update the visual representation of the slider
   updateProgressBar(duration);
 });
 
 function updateProgressBar(duration) {
-  progressBar.style.width = "0";
-
   audioElement.addEventListener("timeupdate", function () {
     const currentTime = audioElement.currentTime;
     const progress = (currentTime / duration) * 100;
-    progressBar.style.width = progress + "%";
+
+    // Use the 'value' method to update the visual representation of the slider
+    seekSlider.slider("value", progress);
   });
 }
 
