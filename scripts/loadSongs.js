2510caba59377c22a40545bbfc992d77df24cc61
win/ubuntu <d-brandenburg@hotmail.de>
2023-11-30 09:25:34 +0100

    fixed

--- scripts/loadSongs.js
+++ scripts/loadSongs.js
@@ -5,53 +5,53 @@ import apikey from "../apikey.js";
 
 const mainArea = document.querySelector(".style-main");
 const options = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": apikey,
-		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-	},
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": apikey,
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
 };
 const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
 
 async function loadSongs(selectedArtist) {
-	mainArea.innerHTML = "";
-	const genreHeading = document.createElement("div");
-	const musicCardArea = document.createElement("div");
-	genreHeading.classList.add("spacer-heading");
-	genreHeading.innerHTML = `
+  mainArea.innerHTML = "";
+  const genreHeading = document.createElement("div");
+  const musicCardArea = document.createElement("div");
+  genreHeading.classList.add("spacer-heading");
+  genreHeading.innerHTML = `
 			<div class="headWithBack">
 				<h2>All found tracks of ${selectedArtist}</h2>
 				<p id="backBtn">Back</p>
 			</div>
 			<hr>
 		`;
-	mainArea.appendChild(genreHeading);
-	musicCardArea.classList.add("music-card-area");
-	mainArea.appendChild(musicCardArea);
-	const backBtn = document.getElementById("backBtn");
-	backBtn.addEventListener("click", () => {
-		loadArtists(state.currentPage);
-		// console.log(`We re currently in the page of ${state.currentPage}`);
-	});
-	try {
-		const response = await fetch(`${url}${selectedArtist}`, options);
-		const data = await response.json();
-		if (data.data && Array.isArray(data.data)) {
-			data.data.forEach((songData) => {
-				const albumId = songData.album.id;
-				const trackId = songData.id;
-				const trackIdLink = songData.link;
-				const artistName = songData.artist.name;
-				if (artistName === selectedArtist) {
-					const musicCard = document.createElement("div");
-					musicCard.classList.add("music-card");
-					musicCard.setAttribute("id", albumId);
-					let title = songData.title_short;
-					const titleLength = title.split("");
-					if (titleLength.length >= 30) {
-						title = title.slice(0, 37) + "...";
-					}
-					musicCard.innerHTML = `
+  mainArea.appendChild(genreHeading);
+  musicCardArea.classList.add("music-card-area");
+  mainArea.appendChild(musicCardArea);
+  const backBtn = document.getElementById("backBtn");
+  backBtn.addEventListener("click", () => {
+    loadArtists(state.currentPage);
+    // console.log(`We re currently in the page of ${state.currentPage}`);
+  });
+  try {
+    const response = await fetch(`${url}${selectedArtist}`, options);
+    const data = await response.json();
+    if (data.data && Array.isArray(data.data)) {
+      data.data.forEach((songData) => {
+        const albumId = songData.album.id;
+        const trackId = songData.id;
+        const trackIdLink = songData.link;
+        const artistName = songData.artist.name;
+        if (artistName === selectedArtist) {
+          const musicCard = document.createElement("div");
+          musicCard.classList.add("music-card");
+          musicCard.setAttribute("id", albumId);
+          let title = songData.title_short;
+          const titleLength = title.split("");
+          if (titleLength.length >= 30) {
+            title = title.slice(0, 37) + "...";
+          }
+          musicCard.innerHTML = `
 						<img src="${songData.album.cover}" alt="">
 						<div class="card-btm">
 							<p>${title}</p>
@@ -75,6 +75,7 @@ async function loadSongs(selectedArtist) {
     console.log(data.data);
   } catch (error) {
     console.error(error);
+  }
 }
 
 export { loadSongs };
