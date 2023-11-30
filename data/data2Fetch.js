386e449d844ea07e299d8ded36ff5545493a2ad6
RealMahu <dannyboehm93@gmail.com>
2023-11-30 19:17:03 +0100

    more genres, more artists

--- data/data2Fetch.js
+++ data/data2Fetch.js
@@ -40,6 +40,26 @@ let genreArtists = [
 		genreArtists: [ "Bob Marley & The Wailers", "Burning Spear", "UB40", "Bunny Wailer", "U-Roy", "Steel Pulse", "Max Romeo", "Alborosie", "Clinton Fearon", "Manu Chao", "Peter Tosh", "Jah Cure", "Maxi Priest", "Sean Paul", "Flox",
 		],
 	},
+	{
+		genreName: "R&B",
+		genreArtists: [ "Usher", "Ne-Yo", "Aaliyah", "Beyoncé", "The Weeknd", "Prince", "Rihanna", "Jason Derulo", "Destiny's Child", "DJ Khaled", "Justin Timberlake", "Tory Lanez", "ZAYN", "Tyler, The Creator", "Missy Elliott" ],
+	},
+	{
+		genreName: "Country",
+		genreArtists: [ "Chris Stapleton", "Carrie Underwood", "Lainey Wilson", "Dolly Parton", "Tim McGraw", "Luke Combs", "Morgan Wallen", "Old Dominion", "Alan Jackson", "Keith Urban", "Shania Twain", "Ashley McBryde", "Johnny Cash", "Hank Williams", "Billy Ray Cyrus" ],
+	},
+	{
+		genreName: "Klassik",
+		genreArtists: ["Gautier Capuçon", "Daniel Barenboim", "Renaud Capuçon", "Sofiane Pamart", "Nelson Freire", "Alexandre Tharaud" ],
+	},
+	{
+		genreName: "Filme/Videospiele",
+		genreArtists: [ "Gustavo Santaolalla", "Hans Zimmer", "Shoi Miyazawa", "SQUARE ENIX MUSIC", "Greg Edmonson", "Brian Tyler", "Inon Zur", "Tyler Bates", "Marcin Przybyłowicz", "Jeremy Soule", "Gareth Coker", "chuck e. myers 'sea'", "Hiroyuki Sawano", "Danny Elfman", "Alexandre Desplat" ],
+	},
+	{
+		genreName: "Soul & Funk",
+		genreArtists: [ "James Brown", "Etta James", "Jackson 5", "Ray Charles", "Kool & The Gang", "Stevie Wonder", "Diana Ross", "Aretha Franklin", "Marvin Gaye", "Otis Redding", "Billy Paul", "Amy Winehouse" ],
+	},
 ];
 
 const genreUrlList = [
@@ -74,6 +94,26 @@ const genreUrlList = [
 	{
 		genre: "Reggae",
 		genreId: "144",
+	},
+	{
+		genre: "R&B",
+		genreId: "165",
+	},
+	{
+		genre: "Country",
+		genreId: "84",
+	},
+	{
+		genre: "Klassik",
+		genreId: "98",
+	},
+	{
+		genre: "Filme/Videospiele",
+		genreId: "173",
+	},
+	{
+		genre: "Soul & Funk",
+		genreId: "169",
 	},
 ];
 
